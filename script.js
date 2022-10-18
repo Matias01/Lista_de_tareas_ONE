a2c01abc2911f1d9f6266ae36cafcf59c2b5a3b8
Matias N.Bergamaschi <matiasbergamaschi@gmail.com>
2022-10-18 15:35:27 -0300

    clase 05 completada

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Immediately Invoked Function Expression IIFE
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 ( () => {
     const btn = document.querySelector("[data-form-btn]");
 
@@ -15,30 +19,12 @@
         titleTask.classList.add("task");
         titleTask.innerText = value;
         
-        const content = `
-        <i class="fas fa-trash-alt trashIcon icon"></i>`
-        // task.innerHTML = content;
-        
         taskContent.appendChild(checkComplete());
         taskContent.appendChild(titleTask);
         task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
         list.appendChild(task);
     }
 
     btn.addEventListener("click", createTask);
-
-    const checkComplete = () => {
-        const i = document.createElement("i");
-        i.classList.add("far", "fa-check-square", "icon");
-        i.addEventListener("click", completeTask);
-        return i;
-    }
-
-    const completeTask = (event) => {
-        const element = event.target;
-        element.classList.toggle("fas");
-        element.classList.toggle("completeIcon")
-        element.classList.toggle("far");
-    }
 })();
-
